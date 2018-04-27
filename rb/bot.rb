7a364817ec44cc776d9ead9a23779d92b1835e62
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-04-26 20:29:48 -0700

    ...

--- rb/bot.rb
+++ rb/bot.rb
@@ -3,8 +3,8 @@ require 'discordrb'
 require 'httparty'
 
 config  = JSON.parse(File.read("token.json"))
-discord-token = config["discord-token"]
-bot = Discordrb::Bot.new token: "#{discord-token}"
+token = config["discord-token"]
+bot = Discordrb::Bot.new token: "#{token}"
 
 bot.message(with_text: ['/coin', '/Coin']) do |event|
 grav = HTTParty.get("https://graviex.net/api/v2/tickers/egembtc.json", :verify => false )
