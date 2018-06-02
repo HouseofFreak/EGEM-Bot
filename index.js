7a79d91abb12e0beaaf78233c98e6bcb3cd91a64
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-02 11:06:17 -0700

    ...

--- index.js
+++ index.js
@@ -530,32 +530,30 @@ bot.on('message',async message => {
 
 	if(message.content == prefix + "info2"){
 		const embed = new Discord.RichEmbed()
-		  .setTitle("This is your title, it can hold 256 characters")
-		  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+		  .setTitle("EGEM Discord Bot info.")
+		  .setAuthor("TheEGEMBot", "https://raw.githubusercontent.com/TeamEGEM/meta/master/images/32x32.png")
 		  /*
 		   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
 		   */
 		  .setColor(0x00AE86)
-		  .setDescription("This is the main body of text, it can hold 2048 characters.")
-		  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-		  .setImage("http://i.imgur.com/yVpymuV.png")
-		  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+		  .setDescription("")
+		  .setFooter("© EGEM.io", "https://raw.githubusercontent.com/TeamEGEM/meta/master/images/32x32.png")
+		  .setThumbnail("https://raw.githubusercontent.com/TeamEGEM/meta/master/images/32x32.png")
 		  /*
 		   * Takes a Date object, defaults to current date.
 		   */
 		  .setTimestamp()
 		  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-		  .addField("This is a field title, it can hold 256 characters",
-		    "This is a field value, it can hold 2048 characters.")
+		  .addField("Bot Address:", "")
 		  /*
 		   * Inline fields may not display as inline if the thumbnail and/or image is too big.
 		   */
-		  .addField("Inline Field", "They can also be inline.", true)
+		  .addField("Balance", "", true)
 		  /*
 		   * Blank field, useful to create some space.
 		   */
 		  .addBlankField(true)
-		  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+		  .addField("Transactions", "", true);
 
 		  message.channel.send({embed});
 	}
