189ff6b5176c9140affb9f78b90f9e7872720041
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-03 07:58:56 -0700

    getblock

--- commands/block.js
+++ commands/block.js
@@ -1,3 +1,6 @@
+function getBlock(){
+				return JSON.parse(fs.readFileSync('./data/block.txt'));
+}
 exports.run = (client, message, args) => {
     message.channel.send("Current EGEM blockchain height is: " + getBlock()).catch(console.error);
 }
