3ea22e50a5e8a7cf1de4dd026a0da3ee7bbc718c
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-04 11:21:13 -0700

    ...

--- index.js
+++ index.js
@@ -635,44 +635,82 @@ if(message.content.startsWith(prefix + "roll")){
 			let array = [1,2,3,4,5,6,7,8,9,10,11,12];
 	    let number = _.sample(array);
 	    let word = randomWord();
-
-			var prize = "You won some EGEM!"
-			let amount = (Math.random() * (0.020 - 0.0100) + 0.0100).toFixed(8);
+			if(number = 12) {
+				var prize = "Jackpot!"
+				let amount = 1;
+			} else if (number < 5) {
+				var prize = "Nothing"
+				let amount = 0;
+			} else {
+				var prize = "You won some EGEM!"
+				let amount = (Math.random() * (0.020 - 0.0100) + 0.0100).toFixed(8);
+			}
 
 			var user = message.author.id;
 			let weiAmount = amount*Math.pow(10,18);
 			let data = getJson();
 			if(Object.keys(data).includes(user)){
 				let address = data[user];
-				const embed = new Discord.RichEmbed()
-		      .setTitle("EGEM Discord Bot.")
-		      .setAuthor("TheEGEMBot", miscSettings.egemspin)
-		      /*
-		       * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-		       */
-		      .setColor(miscSettings.okcolor)
-		      .setDescription("EGEM Dice Game:")
-		      .setFooter("© EGEM.io", miscSettings.img32x32)
-		      .setThumbnail(miscSettings.dice32)
-		      /*
-		       * Takes a Date object, defaults to current date.
-		       */
-		      .setTimestamp()
-		      .setURL("https://github.com/TeamEGEM/EGEM-Bot")
-		      .addField("The dice hit the table and you get:", number)
-		      .addField("Roll Prize:", prize)
-					.addField("EGEM:", amount)
-		      .addField("And the random word for this roll is:", word + ".", true);
+				if(number = 12) {
+					const embed = new Discord.RichEmbed()
+						.setTitle("EGEM Discord Bot.")
+						.setAuthor("TheEGEMBot", miscSettings.egemspin)
+						/*
+						 * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+						 */
+						.setColor(miscSettings.okcolor)
+						.setDescription("EGEM Dice Game:")
+						.setFooter("© EGEM.io", miscSettings.img32x32)
+						.setThumbnail(miscSettings.dice32)
+						/*
+						 * Takes a Date object, defaults to current date.
+						 */
+						.setTimestamp()
+						.setURL("https://github.com/TeamEGEM/EGEM-Bot")
+						.addField("The dice hit the table and you get:", number)
+						.addField("Roll Prize:", prize)
+						.addField("EGEM:", amount)
+						.addField("And the random word for this roll is:", word + ".", true);
 
-		      message.channel.send({embed})
+						message.channel.send({embed})
 
-					sendCoins(address,weiAmount,message,1); // main function
-					// Adds the user to the set so that they can't talk for x
-					rollcooldown.add(message.author.id);
-					setTimeout(() => {
-						// Removes the user from the set after a minute
-						rollcooldown.delete(message.author.id);
-					}, miscSettings.cdroll);
+						sendCoins(address,weiAmount,message,1); // main function
+						// Adds the user to the set so that they can't talk for x
+						rollcooldown.add(message.author.id);
+						setTimeout(() => {
+							// Removes the user from the set after a minute
+							rollcooldown.delete(message.author.id);
+						}, miscSettings.cdroll);
+				} else if (number < 5) {
+					const embed = new Discord.RichEmbed()
+						.setTitle("EGEM Discord Bot.")
+						.setAuthor("TheEGEMBot", miscSettings.egemspin)
+						/*
+						 * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+						 */
+						.setColor(miscSettings.okcolor)
+						.setDescription("EGEM Dice Game:")
+						.setFooter("© EGEM.io", miscSettings.img32x32)
+						.setThumbnail(miscSettings.dice32)
+						/*
+						 * Takes a Date object, defaults to current date.
+						 */
+						.setTimestamp()
+						.setURL("https://github.com/TeamEGEM/EGEM-Bot")
+						.addField("The dice hit the table and you get:", number)
+						.addField("Roll Prize:", prize)
+						.addField("EGEM:", amount)
+						.addField("And the random word for this roll is:", word + ".", true);
+
+						message.channel.send({embed})
+
+						// Adds the user to the set so that they can't talk for x
+						rollcooldown.add(message.author.id);
+						setTimeout(() => {
+							// Removes the user from the set after a minute
+							rollcooldown.delete(message.author.id);
+						}, miscSettings.cdroll);
+				}
 
 			} else {
 				const embed = new Discord.RichEmbed()
