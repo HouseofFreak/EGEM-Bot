7ac9a370c9636a062be5e4d2c56261cae5a6c2b9
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-04 13:50:52 -0700

    ...

--- commands/block.js
+++ commands/block.js
@@ -6,6 +6,7 @@ const miscSettings = require("../cfgs/settings.json");
 function getBlock(){ return JSON.parse(fs.readFileSync('./data/block.txt'));}
 
 exports.run = (client, message, args) => {
+  var getblock = getBlock();
   const embed = new Discord.RichEmbed()
     .setTitle("EGEM Discord Bot.")
     .setAuthor("TheEGEMBot", miscSettings.egemspin)
@@ -21,7 +22,7 @@ exports.run = (client, message, args) => {
      */
     .setTimestamp()
     .setURL("https://github.com/TeamEGEM/EGEM-Bot")
-    .addField("The the most recent block is:", "["+number+"](http://explorer.egem.io/block/" +getBlock()+ ")")
+    .addField("The the most recent block is:", "["+getblock+"](http://explorer.egem.io/block/" +getblock+ ")")
 
     message.channel.send({embed})
 }
