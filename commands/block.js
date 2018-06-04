014ae09c00c9edaaf59e07b88f78d2294c5c9cad
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-04 08:12:39 -0700

    block update

--- commands/block.js
+++ commands/block.js
@@ -1,8 +1,27 @@
 
 const fs = require("fs");
+const Discord = require("discord.js");
+const botSettings = require("../config.json");
+const miscSettings = require("../cfgs/settings.json");
+function getBlock(){ return JSON.parse(fs.readFileSync('./data/block.txt'));}
 
-function getBlock(){ return JSON.parse(fs.readFileSync('./data/block.txt'));
-}
 exports.run = (client, message, args) => {
-    message.channel.send("Current EGEM blockchain height is: " + getBlock()).catch(console.error);
+  const embed = new Discord.RichEmbed()
+    .setTitle("EGEM Discord Bot.")
+    .setAuthor("TheEGEMBot", miscSettings.egemspin)
+    /*
+     * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+     */
+    .setColor(miscSettings.okcolor)
+    .setDescription("Current Blockchain Height:")
+    .setFooter("© EGEM.io", miscSettings.img32x32)
+    .setThumbnail(miscSettings.img32shard)
+    /*
+     * Takes a Date object, defaults to current date.
+     */
+    .setTimestamp()
+    .setURL("https://github.com/TeamEGEM/EGEM-Bot")
+    .addField("The the most recent block is:",  getBlock())
+
+    message.channel.send({embed})
 }
