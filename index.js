4099760c33e3b560f6a9ddbdebf6e0f7340d9a2e
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-05 07:33:20 -0700

    ...

--- index.js
+++ index.js
@@ -577,6 +577,23 @@ bot.on('message',async message => {
 		message.channel.send("Total list of registered and online users are **" + onlineAndRegister+ "**.");
 	}
 
+	if(message.content == prefix + "whatami"){
+		message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
+		.then(() => {
+		  message.channel.awaitMessages(response => response.content === 'test', {
+		    max: 1,
+		    time: 30000,
+		    errors: ['time'],
+		  })
+		  .then((collected) => {
+		      message.channel.send(`The collected message was: ${collected.first().content}`);
+		    })
+		    .catch(() => {
+		      message.channel.send('There was no collected message that passed the filter within the time limit!');
+		    });
+		});
+	}
+
 /*
  * Dice Game.
  */
