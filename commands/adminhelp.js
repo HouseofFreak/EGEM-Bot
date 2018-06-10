b5b297b8837a4584f94b8516853a8a0255cf97e0
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-10 05:52:47 -0700

    admin command list update

--- commands/adminhelp.js
+++ commands/adminhelp.js
@@ -22,8 +22,8 @@ exports.run = (client, message, args) => {
      */
     .setTimestamp()
     .setURL("https://github.com/TeamEGEM/EGEM-Bot")
-    .addField(prefix+"sendToAddress <address> <amount>", "send EGEM to the following address.")
-    .addField(prefix+"tip <name> <amount>", "send EGEM to the following user.")
+    .addField(prefix+"sendToAddress <address> <amount>", "send EGEM to the following address. MAX 100/per TX")
+    .addField(prefix+"tip <name> <amount>", "send EGEM to the following user. MAX 100/per TX")
     .addField(prefix+"rain <amount>", "send EGEM to all registered and online address's.")
     .addField(prefix+"sprinkle", "send 1-10 EGEM to all registered and online address's")
     .addField(prefix+"downpour", "send 10-100 EGEM to all registered and online address's")
@@ -31,6 +31,7 @@ exports.run = (client, message, args) => {
     .addField(prefix+"onlinetotal", "see the list of every online user.")
     .addField(prefix+"forecast <amount> <numOfHrs>", "rain will be after N hours.")
     .addField(prefix+"purge <number>", "cleans last X posts from channel.")
+    .addField(prefix+"motd <message>", "sets the MOTD message.")
 
     message.channel.send({embed})
 }
